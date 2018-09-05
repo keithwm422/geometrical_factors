a974dd3ac6dd0afe0b9b659c3f5d37e5645ee1f9
mcbride.342 <mcbride.342@poe.asc.ohio-state.edu>
2018-09-05 12:54:02 -0400

    file to start off with calculation of geometrical factor for plane detector geometry

---
+++ detector.C
@@ -0,0 +1,25 @@
+// This code is a MC to evaluate the geometrical factor or "effective area"
+// of a given detector geometry. 
+// First, get back that for a planar detector that observes only half the sky
+
+#include <iostream>
+#include <fstream> 
+
+using namespace std;
+
+
+void detector(){
+  //generate the detector positions
+
+  // generate the random trajectory of the particle
+
+  // if the particle hits the dectector, record
+
+  // if particle does not, skip
+
+  // divide the two?
+  
+
+
+
+}
